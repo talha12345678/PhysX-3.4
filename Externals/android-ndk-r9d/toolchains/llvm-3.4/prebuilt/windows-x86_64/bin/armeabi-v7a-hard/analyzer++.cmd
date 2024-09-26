9ac3e9de743f1de0cf98518d556b90e974370d61
talha12345678 <43684976+talha12345678@users.noreply.github.com>
2024-09-26 23:30:30 +0500

    Add ndk-r9d

---
+++ Externals/android-ndk-r9d/toolchains/llvm-3.4/prebuilt/windows-x86_64/bin/armeabi-v7a-hard/analyzer++.cmd
@@ -0,0 +1,10 @@
+@echo off
+if "%1" == "-cc1" goto :L
+%~dp0\..\clang++.exe -target armv7-none-linux-androideabi %*
+if ERRORLEVEL 1 exit /b 1
+goto :done
+:L
+rem target/triple already spelled out.
+%~dp0\..\clang++.exe %*
+if ERRORLEVEL 1 exit /b 1
+:done
